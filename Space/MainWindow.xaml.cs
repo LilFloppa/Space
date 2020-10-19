3b0b1dfbb37c585c7b6a4a57d354563c7768ab16
Nikita Skrynnik <nikita.nstu@gmail.com>
2020-10-19 21:54:04 +0700

    Template

---
+++ Space/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Space
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Image image = new Image();
+
+        ScrollingBackground sbg;
+       
+
+        bool isRunning = true;
+
+        Point MousePos;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BitmapImage bitmap = new BitmapImage(new Uri("pack://application:,,,/SeamlessBackground.png"));
+            image.Source = bitmap;
+
+            image.Height = bitmap.Height;
+            image.Width = bitmap.Width;
+            sbg = new ScrollingBackground(image, 4.0);
+
+            Thread gameThread = new Thread(MainLoop);
+            gameThread.Start();
+        }
+
+        private void MainLoop()
+        {
+            bool running = false;
+            Dispatcher.Invoke(() => running = isRunning);
+
+            while (running)
+            {
+                Input();
+                Update();
+                Draw();
+                Dispatcher.Invoke(() => running = isRunning);
+            }
+
+            Dispatcher.Invoke(() => Close());
+        }
+
+        private void Input()
+        {
+        }
+
+        private void Update()
+        {
+            // Полностью обновлять состояние игры
+            sbg.Update(0.01);
+        }
+
+        private void Draw()
+        {
+            // Отрисовывать все на канвас
+            Dispatcher.Invoke(() =>
+            {
+                Scene.Children.Clear();
+                sbg.Draw(Scene);
+
+            });
+        }
+
+        private void CanvasOnMouseMove(object sender, MouseEventArgs e)
+        {
+            //MousePos = e.GetPosition(this);
+           // Console.WriteLine("{0} {1}" , MousePos.X.ToString(), MousePos.Y.ToString());
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            isRunning = false;
+        }
+    }
+}
---
+++ Space/ScrollingBackground.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Space
+{
+    class ScrollingBackground
+    {
+        public double Velocity { get; private set; } = 0.0;
+        public double Offset { get; private set; } = 0.0;
+
+        public Image Background1 { get; private set; }
+        public Image Background2 { get; private set; }
+
+        public double Width { get; private set; }
+        public double Height { get; private set; }
+        
+        public ScrollingBackground(Image background, double velocity)
+        {
+            Velocity = velocity;
+
+            Background1 = new Image();
+            Background1.Source = background.Source;
+
+            Background2 = new Image();
+            Background2.Source = background.Source;
+
+            Width = background.Width;
+            Height = background.Height;
+        }
+
+        public void Update(double dt)
+        {
+            Offset += Velocity * dt;
+
+            if (Offset >= Height)
+                Offset = 0.0;
+        }
+
+        public void Draw(Canvas scene)
+        {
+            Canvas.SetTop(Background1, Offset - Height + 1);
+            Canvas.SetTop(Background2, Offset);
+
+            scene.Children.Add(Background1);
+            scene.Children.Add(Background2);
+        }
+    }
+}
