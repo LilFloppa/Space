7ad874f45b642cd1f3b90af3f2346c62a532f33e
IgorMuramshchikov <igor.muram@gmail.com>
2021-01-17 22:51:30 +0700

    Add some functions
    1. Collision of a ship with an asteroid and the end of the game after it.
    2. Display of the accumulated points and progress bar.
    3. Completion of the level when the required number of points is reached.
    4. Transition to another level.
    5. Exit button.

--- Space/Actors/Ship.cs
+++ Space/Actors/Ship.cs
@@ -5,115 +5,126 @@ using System.Windows.Input;
 
 namespace Space
 {
-	class ShipController : IActorController
-	{
-		public IActor Owner { get; set; } = null;
-		public Point Direction { get; set; } = new Point(0.0, 0.0);
+   class ShipController : IActorController
+   {
+      public IActor Owner { get; set; } = null;
+      public Point Direction { get; set; } = new Point(0.0, 0.0);
 
-		public ShipController(Ship owner) => Owner = owner;
- 
-		public void OnUpdate(double dt)
-		{
-			Direction = new Point(0.0, 0.0);
+      public ShipController(Ship owner) => Owner = owner;
 
-			if (Keyboard.IsKeyDown(Key.W))
-				Direction = new Point(Direction.X, Direction.Y - 1.0);
+      public void OnUpdate(double dt)
+      {
+         Direction = new Point(0.0, 0.0);
 
-			if (Keyboard.IsKeyDown(Key.S))
-				Direction = new Point(Direction.X, Direction.Y + 1.0);
+         if (Keyboard.IsKeyDown(Key.W))
+            Direction = new Point(Direction.X, Direction.Y - 1.0);
 
-			if (Keyboard.IsKeyDown(Key.A))
-				Direction = new Point(Direction.X - 1.0, Direction.Y);
+         if (Keyboard.IsKeyDown(Key.S))
+            Direction = new Point(Direction.X, Direction.Y + 1.0);
 
-			if (Keyboard.IsKeyDown(Key.D))
-				Direction = new Point(Direction.X + 1.0, Direction.Y);
-		}
-	}
+         if (Keyboard.IsKeyDown(Key.A))
+            Direction = new Point(Direction.X - 1.0, Direction.Y);
 
-	struct ShipSpecs
-	{
-		public int Level;
-		public int HP;
-		public int Damage;
-		public int MaxHP;
-		public double Velocity;
-		public double Cooldown;
-	}
+         if (Keyboard.IsKeyDown(Key.D))
+            Direction = new Point(Direction.X + 1.0, Direction.Y);
+      }
+   }
 
-	class Ship : Actor
-	{
-		public ShipController Controller { get; set; } = null;
+   struct ShipSpecs
+   {
+      public int Level;
+      public int HP;
+      public int Damage;
+      public int MaxHP;
+      public double Velocity;
+      public double Cooldown;
+   }
 
-		public int Level { get; set; }
-		public int HP { get; set; }
-		public int Damage { get; set; }
-		public int MaxHP { get; set; }
-		public double Velocity { get; set; }
-		public double Cooldown { get; set; }
+   class Ship : Actor
+   {
+      public ShipController Controller { get; set; } = null;
 
-		public Ship(Scene scene, DrawComponent dc, TransformComponent tc, ShipSpecs specs) : base(scene, tc, dc)
-		{
-			Controller = new ShipController(this);
-			Text = new TextComponent(HP.ToString());
+      public int Level { get; set; }
+      public int HP { get; set; }
+      public int Damage { get; set; }
+      public int MaxHP { get; set; }
+      public double Velocity { get; set; }
+      public double Cooldown { get; set; }
 
-			Level = specs.Level;
-			Velocity = specs.Velocity;
-			HP = specs.HP;
-			Damage = specs.Damage;
-			MaxHP = specs.MaxHP;
-			Cooldown = specs.Cooldown;
-		}
+      public Ship(Scene scene, DrawComponent dc, TransformComponent tc, ShipSpecs specs) : base(scene, tc, dc)
+      {
+         Controller = new ShipController(this);
+         Text = new TextComponent(HP.ToString());
 
-		public override void OnUpdate(double dt)
-		{
-			UpdateTransform(dt);
-			Attack(dt);
-			Text.Text = HP.ToString();
-		}
+         Level = specs.Level;
+         Velocity = specs.Velocity;
+         HP = specs.HP;
+         Damage = specs.Damage;
+         MaxHP = specs.MaxHP;
+         Cooldown = specs.Cooldown;
+      }
 
-		void UpdateTransform(double dt)
-		{
-			Controller.OnUpdate(dt);
-			Point Offset = new Point(Controller.Direction.X * Velocity * dt, Controller.Direction.Y * Velocity * dt);
+      public override void OnUpdate(double dt)
+      {
+         UpdateTransform(dt);
+         Attack(dt);
+         Text.Text = HP.ToString();
+      }
 
-			BC.AddOffset(Offset);
+      void UpdateTransform(double dt)
+      {
+         Controller.OnUpdate(dt);
+         Point Offset = new Point(Controller.Direction.X * Velocity * dt, Controller.Direction.Y * Velocity * dt);
 
-			if (BC.BoundingRect.Top < 0.0)
-				BC.SetPosition(new Point(BC.BoundingRect.X, 0.0));
+         BC.AddOffset(Offset);
 
-			if (BC.BoundingRect.Bottom > Scene.Game.Window.Height)
-				BC.SetPosition(new Point(BC.BoundingRect.X, Scene.Game.Window.Height - BC.BoundingRect.Height));
+         if (BC.BoundingRect.Top < 0.0)
+            BC.SetPosition(new Point(BC.BoundingRect.X, 0.0));
 
-			if (BC.BoundingRect.Left < 0.0)
-				BC.SetPosition(new Point(0.0, BC.BoundingRect.Y));
+         if (BC.BoundingRect.Bottom > Scene.Game.Window.Height)
+            BC.SetPosition(new Point(BC.BoundingRect.X, Scene.Game.Window.Height - BC.BoundingRect.Height));
 
-			if (BC.BoundingRect.Right > Scene.Game.Window.Width)
-				BC.SetPosition(new Point(Scene.Game.Window.Width - BC.BoundingRect.Width, BC.BoundingRect.Y));
+         if (BC.BoundingRect.Left < 0.0)
+            BC.SetPosition(new Point(0.0, BC.BoundingRect.Y));
 
-			TC.SetPosition(new Point(BC.BoundingRect.X + BC.BoundingRect.Width / 2.0, BC.BoundingRect.Y + BC.BoundingRect.Height / 2.0));
-		}
+         if (BC.BoundingRect.Right > Scene.Game.Window.Width)
+            BC.SetPosition(new Point(Scene.Game.Window.Width - BC.BoundingRect.Width, BC.BoundingRect.Y));
 
-		void Attack(double dt)
-		{
-			if (Cooldown <= 0.0)
-			{
-				LaserSpecs specs = new LaserSpecs();
-				specs.Direction = new Point(0.0, -1.0);
-				specs.Damage = Damage;
-				specs.LifeSpan = 2.0;
-				specs.Velocity = 500.0;
-				Laser laser1 = new Laser(Scene, new TransformComponent(TC.Position.X - 18, TC.Position.Y), specs);
-				Laser laser2 = new Laser(Scene, new TransformComponent(TC.Position), specs);
-				Laser laser3 = new Laser(Scene, new TransformComponent(TC.Position.X + 18, TC.Position.Y), specs);
-				Scene.NewActors.Add(laser1);
-				Scene.NewActors.Add(laser2);
-				Scene.NewActors.Add(laser3);
-				Cooldown = 0.05;
-			}
-			else
-			{
-				Cooldown -= dt;
-			}			
-		}
-	}
+         TC.SetPosition(new Point(BC.BoundingRect.X + BC.BoundingRect.Width / 2.0, BC.BoundingRect.Y + BC.BoundingRect.Height / 2.0));
+      }
+
+      void Attack(double dt)
+      {
+         if (Cooldown <= 0.0)
+         {
+            LaserSpecs specs = new LaserSpecs();
+            specs.Direction = new Point(0.0, -1.0);
+            specs.Damage = Damage;
+            specs.LifeSpan = 2.0;
+            specs.Velocity = 500.0;
+            Laser laser1 = new Laser(Scene, new TransformComponent(TC.Position.X - 18, TC.Position.Y), specs);
+            Laser laser2 = new Laser(Scene, new TransformComponent(TC.Position), specs);
+            Laser laser3 = new Laser(Scene, new TransformComponent(TC.Position.X + 18, TC.Position.Y), specs);
+            Scene.NewActors.Add(laser1);
+            Scene.NewActors.Add(laser2);
+            Scene.NewActors.Add(laser3);
+            Cooldown = 0.05;
+         }
+         else
+         {
+            Cooldown -= dt;
+         }
+      }
+
+      public void GetDamage(int damage)
+      {
+         HP -= damage;
+         if (HP <= 0.0)
+            MustBeDestroyed = true;
+      }
+
+      public void OnDestroy()
+      {
+      }
+   }
 }
--- Space/Game/Game.cs
+++ Space/Game/Game.cs
@@ -4,102 +4,147 @@ using System.Windows;
 
 namespace Space
 {
-	public enum GameState
-	{
-		MainMenu,
-		Start,
-		InProgress,
-		GameOver,
-		Exit
-	}
+   public enum GameState
+   {
+      MainMenu,
+      Start,
+      InProgress,
+      GameOver,
+      Exit
+   }
 
-	class Game
-	{
-		public MainWindow Window { get; set; }
-		public Scene Scene { get; set; }
-		public GameState State { get; set; } = GameState.MainMenu;
+   class Game
+   {
+      public MainWindow Window { get; set; }
+      public Scene Scene { get; set; }
+      public GameState State { get; set; } = GameState.MainMenu;
 
-		public ScrollingBackground SBG { get; set; } = null;
+      public ScrollingBackground SBG { get; set; } = null;
 
-		public PhysicsManager PM { get; set; } = new PhysicsManager();
-		public AssetManager AM { get; set; } = new AssetManager();
-		public CollisionResolveManager CRM { get; set; } = new CollisionResolveManager();
+      public PhysicsManager PM { get; set; } = new PhysicsManager();
+      public AssetManager AM { get; set; } = new AssetManager();
+      public CollisionResolveManager CRM { get; set; } = new CollisionResolveManager();
 
-		public uint MaxAsteroidCount = 20;
-		public uint AsteroidCount = 0;
-		public double AsteroidCooldown = 0.0;
+      public uint MaxAsteroidCount = 20;
+      public uint AsteroidCount = 0;
+      public double AsteroidCooldown = 0.0;
 
-		public int Score { get; set; }
+      public int Level { get; set; } = 1;
+      public int Score { get; set; } = 0;
+      public int MaxScore { get; set; } = 5000;
 
-		public Game(MainWindow window)
-		{
-			Window = window;
-			Scene = new Scene(this);
-		}
+      public Game(MainWindow window)
+      {
+         Window = window;
+         Scene = new Scene(this);
+      }
 
-		public void Update(double dt)
-		{
-			SBG.Update(dt);
-			PM.CheckCollisions();
+      public void Update(double dt)
+      {
+         SBG.Update(dt);
+         PM.CheckCollisions();
 
-			Console.WriteLine(PM.BoxComponents.Count);
-			foreach (var collision in PM.Collisions)
-				CRM.ResolveCollision(collision);
+         Console.WriteLine(PM.BoxComponents.Count);
+         foreach (var collision in PM.Collisions)
+            CRM.ResolveCollision(collision);
 
-			Scene.Update(dt);
+         Scene.Update(dt);
 
-			CreateAsteroid(dt);
-		}
+         CreateAsteroid(dt);
+      }
 
-		public void Start()
-		{
-			// Background
-			SBG = new ScrollingBackground(AM.GetTexture("Background.png"), 100.0);
+      public void Start()
+      {
+         Scene.endGame = false;
 
-			// Player
-			ShipSpecs specs = new ShipSpecs();
-			specs.Level = 1;
-			specs.Velocity = 400.0;
-			specs.HP = 5;
-			specs.MaxHP = 50;
-			specs.Damage = 15;
-			specs.Cooldown = 0.3;
-			Ship player = new Ship(Scene, new DrawComponent(AM.GetTexture("Ship.png"), new Size(64.0, 64.0)), new TransformComponent(new Point(200.0, 700.0)), specs);
-			Scene.NewActors.Add(player);
-			PM.CreateBoxComponent(new Size(64.0, 64.0), player);
+         // Background
+         SBG = new ScrollingBackground(AM.GetTexture("Background.png"), 100.0);
 
-			State = GameState.InProgress;
-		}
+         // Player
+         ShipSpecs specs = new ShipSpecs();
+         specs.Level = 1;
+         specs.Velocity = 400.0;
+         specs.HP = 5;
+         specs.MaxHP = 50;
+         specs.Damage = 15;
+         specs.Cooldown = 0.3;
+         Ship player = new Ship(Scene, new DrawComponent(AM.GetTexture("Ship.png"), new Size(64.0, 64.0)), new TransformComponent(new Point(200.0, 700.0)), specs);
+         Scene.NewActors.Add(player);
+         PM.CreateBoxComponent(new Size(64.0, 64.0), player);
 
-		void CreateAsteroid(double dt)
-		{
-			if (AsteroidCount < MaxAsteroidCount)
-			{
-				if (AsteroidCooldown < 0.0)
-				{
-					Random random = new Random();
-					AsteroidSpecs specs = new AsteroidSpecs();
-					specs.Direction = new Point(0.0, 1.0);
-					specs.Velocity = random.Next(150, 200);
-					specs.RotationVelocity = random.NextDouble() * 60 + 20;
-					specs.HP = random.Next(300, 900);
+         State = GameState.InProgress;
 
-					int x = random.Next(0, (int)Window.Width);
-					int y = random.Next(-500, -100);
-					TransformComponent TC = new TransformComponent((double)x, (double)y);
-					Asteroid asteroid = new Asteroid(Scene, new DrawComponent(AM.GetTexture("Asteroid.png"), new Size(100.0, 100.0)), TC, specs);
+         Window.ScoreBar.Maximum = MaxScore;
+      }
 
-					Scene.NewActors.Add(asteroid);
-					PM.CreateBoxComponent(new Size(100.0, 100.0), asteroid);
+      public void GameOver(int score, bool success)
+      {
+         Window.gameOverMenu.GameOverLabel.Content = "Game Over! You " + (success ? "win!" : "lose!");
+         Window.gameOverMenu.ResultLabel.Content = "Result: " + Score;
 
-					AsteroidCount++;
-					AsteroidCooldown = 1.5;
-				}
-				else
-				{
-					AsteroidCooldown -= dt;
-				}
-			}
-		}
-	}
+         Score = 0;
+         Window.Score.Content = "Score: 0";
+         Window.ScoreBar.Value = 0;
+
+         State = GameState.GameOver;
+         Window.Grid.Children.Remove(Window.menu);
+         Window.Grid.Children.Add(Window.gameOverMenu);
+
+         if (success)
+         {
+            Level++;
+            MaxScore = 5000 * Level;
+            Window.ScoreBar.Maximum = MaxScore;
+            Window.menu.LevelLabel.Content = "Level: " + Level;
+            Window.menu.TargetLabel.Content = "Target: " + MaxScore;
+
+            PM.BoxComponents.Clear();
+            Scene.Actors.Clear();
+         }
+      }
+
+      void CreateAsteroid(double dt)
+      {
+         if (AsteroidCount < MaxAsteroidCount)
+         {
+            if (AsteroidCooldown < 0.0)
+            {
+               Random random = new Random();
+               AsteroidSpecs specs = new AsteroidSpecs();
+               specs.Direction = new Point(0.0, 1.0);
+               specs.Velocity = random.Next(150, 200);
+               specs.RotationVelocity = random.NextDouble() * 60 + 20;
+               specs.HP = random.Next(300, 900);
+
+               int x = random.Next(0, (int)Window.Width);
+               int y = random.Next(-500, -100);
+               TransformComponent TC = new TransformComponent((double)x, (double)y);
+               Asteroid asteroid = new Asteroid(Scene, new DrawComponent(AM.GetTexture("Asteroid.png"), new Size(100.0, 100.0)), TC, specs);
+
+               Scene.NewActors.Add(asteroid);
+               PM.CreateBoxComponent(new Size(100.0, 100.0), asteroid);
+
+               AsteroidCount++;
+               AsteroidCooldown = 1.5;
+            }
+            else
+            {
+               AsteroidCooldown -= dt;
+            }
+         }
+      }
+
+      public void ScoreChanged(int score)
+      {
+         Score += score;
+
+         if (Score < MaxScore)
+         {
+            Window.ScoreBar.Value = Score;
+            Window.Score.Content = "Score: " + Score;
+         }
+         else
+            GameOver(Score, true);
+      }
+   }
 }
